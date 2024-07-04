9bd2437bb98c5c72966fb34f82700aabccd67b79
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-04 18:56:59 +0800

    Implemented route guards for sign in and sign up pages so that it can't be accessed when the user is logged in

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,8 +1,14 @@
 import AuthForm from '@/components/AuthForm';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { getLoggedInUser } from '@/lib/actions/userActions';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default function SignIn() {
+export default async function SignIn() {
+  const loggedInUser = await getLoggedInUser();
+
+  if (loggedInUser?.userId) redirect('/');
+
   return (
     <MaxWidthWrapper className='w-full h-screen flex flex-col items-center justify-center'>
       <AuthForm type='sign-in' />
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,14 @@
 import AuthForm from '@/components/AuthForm';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import { getLoggedInUser } from '@/lib/actions/userActions';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default function SignUp() {
+export default async function SignUp() {
+  const loggedInUser = await getLoggedInUser();
+
+  if (loggedInUser?.userId) redirect('/');
+
   return (
     <MaxWidthWrapper className='w-full min-h-screen flex flex-col items-center justify-center'>
       <AuthForm type='sign-up' />
