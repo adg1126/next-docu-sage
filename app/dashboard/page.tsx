b2c0a8a74f80fa44268e21042d0bcda2864fb9a1
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-04 20:46:08 +0800

    Implemented route guard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,11 @@
+import { getLoggedInUser } from '@/lib/actions/userActions';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default function Dashboard() {
-  return <div></div>;
+export default async function Dashboard() {
+  const loggedInUser = await getLoggedInUser();
+
+  if (!loggedInUser?.userId) redirect('/');
+
+  return <div>Dashboard</div>;
 }
