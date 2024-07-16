ae7ea3363778951453b8eb5ff1575f6f6286c67c
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-16 10:40:09 +0800

    Setup react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import 'simplebar-react/dist/simplebar.min.css';
+import ReactQueryProvider from '@/components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,7 +21,9 @@ export default function RootLayout({
       lang='en'
       className='light'
     >
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
